f3cc04ae0a0106b4803ced0f3c89aabff4e25f66
Ahmedkamal295 <Ahmedkamal21995@gmail.com>
2020-05-08 13:26:04 -0700

    first commit

--- Payment/ViewController.swift
+++ Payment/ViewController.swift
@@ -5,16 +5,29 @@
 //  Created by Ahmed on 5/7/20.
 //  Copyright © 2020 Ahmed. All rights reserved.
 //
-
 import UIKit
+import Stripe
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, STPAddCardViewControllerDelegate {
+    
+    func addCardViewControllerDidCancel(_ addCardViewController: STPAddCardViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
+    @IBAction func pay(_ sender: Any) {
+        let config = STPPaymentConfiguration()
+        config.requiredBillingAddressFields = .full
+        let viewController = STPAddCardViewController(configuration: config, theme: .default())
+        viewController.delegate = self
+        let navigationController = UINavigationController(rootViewController: viewController)
+        present(navigationController, animated: true, completion: nil)
+    }
 }
 
